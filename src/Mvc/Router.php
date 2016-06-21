4249c5cd9cefe3ba2a821c798d31688109356c0a
Gustavo Binow <gbinow@gmail.com>
2016-06-21 14:41:28 -0300

    Ajustando arquivos de rotas

--- src/Mvc/Router.php
+++ src/Mvc/Router.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 namespace Easyme\Mvc;
@@ -14,30 +14,46 @@ use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Exception;
 
 class Router{
-    
+
+    private $byPassList = [];
+
+    public function allow($route , $parameters){
+        $this->byPassList[] = [$route,$parameters];
+    }
+
     /**
-     * 
+     *
      * @param string $uri
      * @return \Easyme\Mvc\Router\Route|boolean
      * @throws \Easyme\Error\NotFoundException
      */
     public function getRoute($uri){
-        
+
         if(!$uri) $uri = '/';
-        
+
+        if($uri != '/'){
+            $uri = preg_replace('/\/+$/', '', $uri);
+        }
+
+        foreach($this->byPassList as $allowedRoute){
+            if (preg_match($allowedRoute[0], $uri)){
+                return $this->_getRoute($allowedRoute[1]);
+            }
+        }
+
         $parts = $uri == '/' ? [''] : explode('/', $uri);
-        
+
         $path = "";
         $searchDirs = [];
 
         foreach($parts as $i=>$part){
-            
-            
+
+
             $relPath = $i > 0 ? "$path/$part" : "";
             $absPath = EFWK_APP_DIR . $relPath;
-            
+
             if(is_dir($absPath) && file_exists($absPath.'/routes.yml')){
-                
+
                 array_unshift($searchDirs, [
                     'path' => $absPath,
                     'namespace' => $relPath
@@ -45,14 +61,14 @@ class Router{
             }
             $path .= $i > 0 ? "/$part" : $part;
         }
-        
+
         $context = new RequestContext();
         $context->fromRequest(SymfonyRequest::createFromGlobals());
-        
+
         foreach($searchDirs as $dir){
 
             try{
-                
+
                 $locator = new FileLocator($dir['path']);
                 $loader = new YamlFileLoader($locator);
                 $collection = $loader->load('routes.yml');
@@ -60,35 +76,44 @@ class Router{
                 $matcher = new UrlMatcher($collection, $context);
 
                 $parameters = $matcher->match($uri);
-                
-                $route = new Route();
-                
-                $ccu = explode(":", $parameters['_ccu']);
-                
-                // A ultima posicao do array eh sempre o metodo a ser executado
-                $route->setAction(array_pop($ccu));
-                // A proxima, eh sempre o nome da ccu
-                $route->setCcu(array_pop($ccu));
-                
-                $route->setNamespace(  $dir['namespace'] . '/' . implode('/', $ccu) );
-                
-                foreach($parameters as $k=>$v){
-                    if($k[0] != '_'){
-                        $route->setParam($k,$v);
-                    }
-                }
-                
-                return $route;
-                
+
+                return $this->_getRoute($parameters, $dir['namespace']);
+
             } catch (Exception $ex) {
                 // Sem saida..
             }
-            
+
         }
-        
-         throw new \Easyme\Error\NotFoundException("Route for $uri not found");
-        
+
+        throw new \Easyme\Error\NotFoundException("Route for $uri not found");
+
     }
-    
-    
+
+    /**
+     *
+     * @param type $parameters
+     * @return Route
+     */
+    private function _getRoute($parameters , $namespace = ''){
+
+        $route = new Route();
+
+        $ccu = explode(":", $parameters['_ccu']);
+
+        // A ultima posicao do array eh sempre o metodo a ser executado
+        $route->setAction(array_pop($ccu));
+        // A proxima, eh sempre o nome da ccu
+        $route->setCcu(array_pop($ccu));
+
+        $route->setNamespace(  $namespace . '/' . implode('/', $ccu) );
+
+        foreach($parameters as $k=>$v){
+            if($k[0] != '_'){
+                $route->setParam($k,$v);
+            }
+        }
+
+        return $route;
+    }
+
 }
--- src/Mvc/Router/Route.php
+++ src/Mvc/Router/Route.php
@@ -1,119 +1,233 @@
-<?php 
+<?php
 
+namespace Easyme\Mvc\Router;
 
-namespace Easyme\Mvc;
+use \Easyme\Error\NotFoundException;
 
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Easyme\Mvc\Router\Route;
+class Route {
 
-use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+    private $namespace;
 
-use Exception;
+    private $ccu;
 
-class Router{
-    
-    private $byPassList = [];
-    
-    public function allow($route , $parameters){
-        $this->byPassList[] = [$route,$parameters];
-    }
-    
+    private $action;
+
+    private $params = array();
+
     /**
-     * 
-     * @param string $uri
-     * @return \Easyme\Mvc\Router\Route|boolean
-     * @throws \Easyme\Error\NotFoundException
+     * O caminho onde esta a Ccu
+     * @var string
      */
-    public function getRoute($uri){
-        
-        if(!$uri) $uri = '/';
-        
-        if($uri != '/'){
-            $uri = preg_replace('/\/+$/', '', $uri);
-        }
-        
-        foreach($this->byPassList as $allowedRoute){
-            if (preg_match($allowedRoute[0], $uri)){
-                return $this->_getRoute($allowedRoute[1]);
-            }
-        }
-        
-        $parts = $uri == '/' ? [''] : explode('/', $uri);
-        
-        $path = "";
-        $searchDirs = [];
+    private $ccuPath;
 
-        foreach($parts as $i=>$part){
-            
-            
-            $relPath = $i > 0 ? "$path/$part" : "";
-            $absPath = EFWK_APP_DIR . $relPath;
-            
-            if(is_dir($absPath) && file_exists($absPath.'/routes.yml')){
-                
-                array_unshift($searchDirs, [
-                    'path' => $absPath,
-                    'namespace' => $relPath
-                ]);
-            }
-            $path .= $i > 0 ? "/$part" : $part;
-        }
-        
-        $context = new RequestContext();
-        $context->fromRequest(SymfonyRequest::createFromGlobals());
-        
-        foreach($searchDirs as $dir){
-
-            try{
-                
-                $locator = new FileLocator($dir['path']);
-                $loader = new YamlFileLoader($locator);
-                $collection = $loader->load('routes.yml');
-
-                $matcher = new UrlMatcher($collection, $context);
-
-                $parameters = $matcher->match($uri);
-                
-                return $this->_getRoute($parameters, $dir['namespace']);
-                
-            } catch (Exception $ex) {
-                // Sem saida..
-            }
-            
-        }
-        
-         throw new \Easyme\Error\NotFoundException("Route for $uri not found");
-        
+    public function getNamespace() {
+        return $this->namespace;
     }
-    
-    /**
-     * 
-     * @param type $parameters
-     * @return Route
-     */
-    private function _getRoute($parameters , $namespace = ''){
-        
-        $route = new Route();
-                
-        $ccu = explode(":", $parameters['_ccu']);
 
-        // A ultima posicao do array eh sempre o metodo a ser executado
-        $route->setAction(array_pop($ccu));
-        // A proxima, eh sempre o nome da ccu
-        $route->setCcu(array_pop($ccu));
+    public function getCcu() {
+        return $this->ccu;
+    }
 
-        $route->setNamespace(  $namespace . '/' . implode('/', $ccu) );
+    public function getAction() {
+        return $this->action;
+    }
 
-        foreach($parameters as $k=>$v){
-            if($k[0] != '_'){
-                $route->setParam($k,$v);
+    public function getParams() {
+        return $this->params;
+    }
+
+    public function setNamespace($ccuPath) {
+        $this->ccuPath = $ccuPath;
+        // Removendo /`s no inicio da string
+        $this->namespace = preg_replace("/^\/*/", '', $ccuPath);
+        // Removendo /`s no final da string
+        $this->namespace = preg_replace("/\/*$/", '', $ccuPath);
+    }
+
+    public function setCcu($ccu) {
+        $this->ccu = $ccu;
+    }
+
+    public function setAction($action) {
+        $this->action = $action;
+    }
+
+    public function setParams($params) {
+        $this->params = $params;
+    }
+
+    public function getParam($key){
+        return $this->params[$key];
+    }
+
+    public function setParam($key,$value){
+        return $this->params[$key] = $value;
+    }
+
+    public function run(){
+
+        $ns = $this->getNamespace();
+
+        /**
+         * Converte uma string abc/def/ghi em Abc\\Def\\Ghi
+         */
+        $ns = implode('\\',array_map(function($part){
+            return ucfirst($part);
+        },explode('/', $ns)));
+
+        /**
+         * Converte dfg em DfgCcu
+         */
+        $ccu = ucfirst($this->getCcu()).'Ccu';
+
+        $action = strtolower($this->getAction()).'Action';
+
+        $params = $this->getParams();
+
+        $className = $ns.'\\'.$ccu;
+
+        if(!class_exists($className))
+            throw new NotFoundException("Class $className not found");
+
+        $instance = new $className;
+
+        if(method_exists($instance, 'init')){
+            if($instance->init() === FALSE){
+                return;
             }
         }
 
-        return $route;
+        if(!method_exists($instance,$action)){
+            throw new NotFoundException("Method $className::$action not found");
+        }
+
+        return call_user_method_array($action, $instance, $params);
+
     }
-    
+
+    public function getCcuPath() {
+        return $this->ccuPath;
+    }
+
+//    public function getNamespace() {
+//        return $this->namespace;
+//    }
+//
+//    public function getCcuName() {
+//        return $this->ccuName;
+//    }
+//    public function getCcuFullName() {
+//        $ccuName = '\\Ccu\\';
+//        if($ns = $this->getNamespace()){
+//            $ccuName .= ucfirst($ns)."\\";
+//        }
+//        $ccuName .= ucfirst($this->getCcuName()) . 'Ccu';
+//        return $ccuName;
+//    }
+//
+//    public function getAction() {
+//        return $this->action;
+//    }
+//
+//    public function getParams() {
+//        return $this->params;
+//    }
+//
+//    public function setNamespace($namespace) {
+//        $this->namespace = $namespace;
+//    }
+//
+//    public function setCcuName($ccuName) {
+//        $this->ccuName = $ccuName;
+//    }
+//
+//    public function setAction($action) {
+//        $this->action = $action;
+//    }
+//
+//    public function setParams($params) {
+//        $this->params = $params;
+//    }
+//
+//    private function _assembly(){
+//
+//        if(!$this->_ccu){
+//
+//            $ccuName = $this->getCcuFullName();
+//
+//            if(!class_exists($ccuName)){
+//                throw new \Exception("Class '$ccuName' not found");
+//            }
+//
+//            $this->_ccu = new $ccuName;
+//
+//            $action = $this->getAction();
+//
+//            $isRestCall = $this->_ccu instanceof \Easyme\Mvc\CcuRest;
+//
+//            if($isRestCall){
+//                if($action == 'index') $action = '';
+//
+//                $action = ucfirst($action);
+//                $method = strtolower($this->request->getMethod());
+//                $actionName = $method .$action. 'Action';
+//
+//                $this->setAction($method.$action);
+//            }else{
+//
+//                $actionName = $action.'Action';
+//            }
+//
+//
+//
+//            /*O metodo nao existe*/
+//            if(!method_exists($ccuName, $actionName)){
+//                throw new \Exception("Method '$actionName' not found in '$ccuName'");
+//            }
+//
+////            if( !$isRestCall ){
+//
+//                $reflection = new \ReflectionMethod ($ccuName, $actionName);
+//                $params = $reflection->getParameters();
+//                $paramsObg = array_filter($params,function($arg){
+//                    return !$arg->isOptional();
+//                });
+//
+//                if( ! (sizeof($this->getParams()) == sizeof($params) || ( (sizeof($this->getParams()) >= sizeof($paramsObg)) && (sizeof($params) > 0) ) )){
+//                    throw new \Exception("Method '$actionName' not found in '$ccuName'");
+//                }
+////            }
+//
+//
+//        }
+//
+//        return true;
+//    }
+//
+//    public function run(){
+//        $this->_assembly();
+//        /*Tenta chamar metodo inicializador da classe*/
+//        if(method_exists($this->_ccu,'init')) $this->_ccu->init();
+//
+//        if( $this->_ccu instanceof \Easyme\Mvc\CcuRest ){
+//            return call_user_method_array('_dispatch', $this->_ccu,array($this->getAction().'Action',$this->getParams()));
+//        }else{
+//            return call_user_method_array($this->getAction().'Action', $this->_ccu, $this->getParams());
+//        }
+//    }
+//
+//    public function exists(){
+//
+//        try{
+//            return $this->test();
+//        } catch (\Exception $ex) {
+////            echo $ex->getMessage();
+//            return false;
+//        }
+//    }
+//    public function test(){
+//        return $this->_assembly();
+//    }
+
+
 }

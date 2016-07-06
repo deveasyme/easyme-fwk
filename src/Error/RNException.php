555f5bb3f385812b9209a3a6847875de668aee6e
Gustavo Binow <gbinow@gmail.com>
2016-07-06 16:07:00 -0300

    Incluindo classe para tratamento de erros de RN

---
+++ src/Error/RNException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: binow
+ * Date: 7/6/16
+ * Time: 4:05 PM
+ */
+
+namespace Easyme\Error;
+
+use Exception;
+
+class RNException extends Exception
+{
+
+}

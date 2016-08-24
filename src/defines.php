59a7686a54f226dd64bd099fe009480621a24601
Gustavo Binow <gbinow@gmail.com>
2016-08-24 09:29:56 -0300

    Trocando defines de constantes para string

--- src/defines.php
+++ src/defines.php
@@ -1,30 +1,30 @@
 <?php
 
-if(!defined(EFWK_ROOT_DIR))
-    define(EFWK_ROOT_DIR,$_SERVER['DOCUMENT_ROOT']);
+if(!defined('EFWK_ROOT_DIR'))
+    define('EFWK_ROOT_DIR',$_SERVER['DOCUMENT_ROOT']);
 
-define(EFWK_VENDOR_DIR, EFWK_ROOT_DIR.'/vendor');
+define('EFWK_VENDOR_DIR', EFWK_ROOT_DIR.'/vendor');
 
-define(EFWK_CONFIG_DIR,EFWK_ROOT_DIR.'/config');
+define('EFWK_CONFIG_DIR',EFWK_ROOT_DIR.'/config');
 
-define(EFWK_LOGS_DIR,EFWK_ROOT_DIR.'/logs');
+define('EFWK_LOGS_DIR',EFWK_ROOT_DIR.'/logs');
 
-define(EFWK_PROXIES_DIR,EFWK_ROOT_DIR.'/proxies');
+define('EFWK_PROXIES_DIR',EFWK_ROOT_DIR.'/proxies');
 
-define(EFWK_UPLOADS_DIR,EFWK_ROOT_DIR.'/uploads');
+define('EFWK_UPLOADS_DIR',EFWK_ROOT_DIR.'/uploads');
 
-define(EFWK_APP_DIR,EFWK_ROOT_DIR.'/server');
+define('EFWK_APP_DIR',EFWK_ROOT_DIR.'/server');
 
-define(EFWK_PUBLIC_DIR,EFWK_ROOT_DIR.'/public');
+define('EFWK_PUBLIC_DIR',EFWK_ROOT_DIR.'/public');
 
 $ini = parse_ini_file(EFWK_CONFIG_DIR.'/app.ini',true);
-define(EFWK_IN_PRODUCTION,!!$ini['Info']['production']);
+define('EFWK_IN_PRODUCTION',!!$ini['Info']['production']);
 
 $db = parse_ini_file(EFWK_CONFIG_DIR.'/database.ini',true);
 $key = $testing ? 'test' : (EFWK_IN_PRODUCTION ? 'production' : 'development');
-define(EFWK_DB_HOST, $db[$key]['host']);
-define(EFWK_DB_USER, $db[$key]['user']);
-define(EFWK_DB_PASSWORD, $db[$key]['password']);
-define(EFWK_DB_DATABASE, $db[$key]['database']);
+define('EFWK_DB_HOST', $db[$key]['host']);
+define('EFWK_DB_USER', $db[$key]['user']);
+define('EFWK_DB_PASSWORD', $db[$key]['password']);
+define('EFWK_DB_DATABASE', $db[$key]['database']);
 
 

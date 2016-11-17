13550f91d6ab1af6013f7d12f11dad588549f88d
Gustavo Binow <gbinow@gmail.com>
2016-11-17 09:27:11 -0200

    Adicionando ForbiddenException

---
+++ src/Error/ForbiddenException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Easyme\Error;
+
+class ForbiddenException extends \Exception{
+    
+    public function __construct($message) {
+         parent::__construct($message, 403, null);
+    }
+
+
+
+}

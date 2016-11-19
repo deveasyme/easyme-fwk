354065eecd9cb8db57af72f5f88abf5e2f4edf30
Gustavo Binow <gbinow@gmail.com>
2016-11-19 17:04:37 -0200

    Ajustando Construtor de Forbidden Exception

--- src/Error/ForbiddenException.php
+++ src/Error/ForbiddenException.php
@@ -4,7 +4,7 @@ namespace Easyme\Error;
 
 class ForbiddenException extends \Exception{
     
-    public function __construct($message) {
+    public function __construct($message = '') {
          parent::__construct($message, 403, null);
     }
 
